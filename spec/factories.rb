723cb9893b77002344ecc2fb4cfe9e8cd1de2850
bodothebest <bodothebest@gmail.com>
2012-09-26 10:40:27 +0200

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Marco Bodini"
+    email    "bodothebest@gmail.com"
+    password "ciaociao"
+    password_confirmation "ciaociao"
+  end
+end
